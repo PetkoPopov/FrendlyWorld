ecb44f14d0282a61d175ff51c6601d0ac90dc7f9
pipjivko <pipjivko@gmail.com>
2023-10-27 22:23:39 +0300

    add login with bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -3,19 +3,22 @@ const mongoose = require('mongoose')
 const bcrypt = require("bcrypt")
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: [true, "need email"] },
+    email: { type: String, required: [true, "need email"], unique: [true, "such email already exists !"] },
     password: { type: String, minLength: [3, "too short"], maxLength: [7, 'too long !'], required: true }
 })
 userSchema.virtual("repeatPassword").set(function (val) {
+   
     if (val !== this.password) {
-        throw new Error('wrong repeat !')
+        throw new Error('wrong repeat from virtual!')
     }
 })
-userSchema.pre('save', async function(){
+userSchema.pre('save', async function () {
     const pass = this.password
     // const salt = await bcrypt.genSalt()
     // console.log(`salt is ${salt}`);
-    const cryptPassword = await bcrypt.hash(pass, 4)
+    const cryptPassword = await bcrypt.hash(pass, 9)
+    ////////////////////   /\
+    // say has no effect bud has ! without him does not crypt
     this.password = cryptPassword
     console.log(`crypt number is ${cryptPassword}`);
     // next()
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 const db = require('../database/connectDB')
 const userModel = require('../models/user')
 const { sign } = require("../lib/jwt")
-
+const bcrypt = require("bcrypt")
 
 exports.register = async (data) => {
     // console.log(data.repeatPassword);
@@ -10,13 +10,14 @@ exports.register = async (data) => {
 }
 exports.login = async (data) => {
     //check if exists
+
     const user = await userModel.find({ email: data.email })
-    
-    if (user && data.password == user[0].password) {
+    const isPassCorrect = await bcrypt.compare(data.password, user[0].password)
+    if (user && isPassCorrect) {
         const token = sign(user[0])
         console.log(token);
 
-        return {token:token,user:user[0]}
+        return { token: token, user: user[0] }
     } else {
         console.log('No User from Services !');
         return ""
@@ -24,7 +25,7 @@ exports.login = async (data) => {
 
 
 }
-exports.logout=()=>{
+exports.logout = () => {
 
-    
+
 }
