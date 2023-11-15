aabfa9304a2b28c5927af29f74afa801821ebc95
pipjivko <pipjivko@gmail.com>
2023-11-15 13:42:15 +0200

    new test

--- src/demoForReplacment.js
+++ src/demoForReplacment.js
@@ -6,7 +6,8 @@ const arr = {
      "ello" : "hello World",
    "Karlovo": "Karlovo",
     "Dimo" :  "hello Dimo",
-    "Hello" : "That is the right !"
+    "Hello" : "That is the right !",
+    "Hello world":"from Petko Popov"
 
 }
 console.log(arr[str]);
